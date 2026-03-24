deb2eda4591f4ea7543af754346ea3b655a81ef3
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-03-24 22:20:27 +0600

    bpl dream 11 basic setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-4xl font-bold text-primary text-center">Hello BPL 11 Dreamers</h2>
+    </div>
+  );
+};
+
+export default App;
